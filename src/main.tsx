44eefc81c031be72252a107e914e265966aeb7ea
Nathan Arthur <nathan@nathanarthur.com>
2025-05-28 09:22:31 -0400

    init routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ConvexReactClient } from "convex/react";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 createRoot(document.getElementById("root")!).render(
   <ConvexAuthProvider client={convex}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ConvexAuthProvider>,
 );
